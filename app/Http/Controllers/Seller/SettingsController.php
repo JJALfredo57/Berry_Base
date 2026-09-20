3f23c1cf11c5210bec3cca04b8c1fbf7d52715a3
JJALfredo57 <josealfredobarrozo75@gmail.com>
2026-09-20 12:41:04 +0800

    Fix seller settings schema import

--- app/Http/Controllers/Seller/SettingsController.php
+++ app/Http/Controllers/Seller/SettingsController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use App\Traits\UploadsFiles;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 
 class SettingsController extends Controller
